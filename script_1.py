5ca8c6af19487235981eaa14271d3321c1cbb931
Thomas Roussel <thomas.roussel@ik.me>
2021-11-03 11:49:24 +0100

    scraped variables

--- script_1.py
+++ script_1.py
@@ -7,5 +7,23 @@ url = 'http://books.toscrape.com/catalogue/sharp-objects_997/index.html'
 page = requests.get(url)
 soup = BeautifulSoup(page.content, 'html.parser')
 
+title = soup.find('h1').text
+
+category = soup.find_all('a')[3].text
+
+review_rating = soup.find_all('p', class_='star-rating')[0].get('class')[1]
+
+product_description = soup.find_all('meta')[2]
+
+upc = soup.find_all('td')[0].text
+
+price_tax_excl = soup.find_all('td')[2].text
+
+price_tax_incl = soup.find_all('td')[3].text
+
+number_available = soup.find_all('td')[5].text[10:13]
+
+image_url = soup.find('img')['src']
+image_url = base_url + image_url
 
 
