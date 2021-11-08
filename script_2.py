4c83ae0d8016051d508909c504d26484bb5207f5
Thomas Roussel <thomas.roussel@ik.me>
2021-11-08 18:24:59 +0100

    update scraping section

--- script_2.py
+++ script_2.py
@@ -64,45 +64,49 @@ get_books_url()
 print(book_list)
 print(len(book_list))
 
-# for _ in book_list:
-#     # ---SCRAPING---
-#     title = soup.find('h1').text
-#
-#     category = soup.find_all('a')[3].text
-#
-#     review_rating = soup.find_all('p', class_='star-rating')[0].get('class')[1]
-#     review_rating = w2n.word_to_num(review_rating)
-#
-#     product_description = soup.find_all('article')[0].find_all('p')[3].text
-#
-#     upc = soup.find_all('td')[0].text
-#
-#     price_tax_excl = soup.find_all('td')[2].text
-#
-#     price_tax_incl = soup.find_all('td')[3].text
-#
-#     number_available = soup.find_all('td')[5].text
-#     number_available = re.sub("[^0-9]", "", number_available)
-#
-#     image_url = soup.find('img')['src']
-#     image_url = urljoin(base_url, image_url)
-#
-#     # ---BOOK INFO TO DICTIONARY---
-#     output = {variable: eval(variable) for variable in ['title',
-#                                                         'category',
-#                                                         'review_rating',
-#                                                         'product_description',
-#                                                         'upc',
-#                                                         'price_tax_excl',
-#                                                         'price_tax_incl',
-#                                                         'number_available',
-#                                                         'image_url',
-#                                                         'url']}
+for item in book_list:
+    # ---SCRAPING---
+    read_page(item)
+
+    title = soup.find('h1').text
+
+    category = soup.find_all('a')[3].text
+
+    review_rating = soup.find_all('p', class_='star-rating')[0].get('class')[1]
+    review_rating = w2n.word_to_num(review_rating)
+
+    product_description = soup.find_all('article')[0].find_all('p')[3].text
+
+    upc = soup.find_all('td')[0].text
+
+    price_tax_excl = soup.find_all('td')[2].text
+
+    price_tax_incl = soup.find_all('td')[3].text
+
+    number_available = soup.find_all('td')[5].text
+    number_available = re.sub("[^0-9]", "", number_available)
+
+    image_url = soup.find('img')['src']
+    image_url = urljoin(item, image_url)
+
+    url = item
+
+    # ---BOOK INFO TO DICTIONARY---
+    output = {variable: eval(variable) for variable in ['title',
+                                                        'category',
+                                                        'review_rating',
+                                                        'product_description',
+                                                        'upc',
+                                                        'price_tax_excl',
+                                                        'price_tax_incl',
+                                                        'number_available',
+                                                        'image_url',
+                                                        'url']}
 
     # ---WRITING CSV & GETTIN IMAGE---
-    # write_info_csv()
-    #
-    # get_image()
+    write_info_csv()
+
+    get_image()
 
 
 print(time.time() - start_time, "seconds")
